3c5c2e8bea96b970640a868bafb8495e7858a240
swapnilmankame1995 <swapnilum95@gmail.com>
2020-07-05 14:40:44 +0530

    added entries to the home page

--- js/data-compile.js
+++ js/data-compile.js
@@ -30,10 +30,10 @@ function adddata(col) {
       if (data == null) {
         break;
       }
-      //console.log(data);
+
       response.push(data);
     }
-
+console.log(selection);
     //PHASE I : RESPONSE READING STAGE END ---------------------------------------------------------------
 
     console.log("Array elements");
@@ -142,7 +142,7 @@ function adddata(col) {
     }
     console.log(disc);
     //----------------------------------COLLECT ROLE WITH "YES" AS A REPLY END-------
-
+document.getElementById("intro").innerHTML = "<h7><i>All entries for</i></h7>" + " " + selection;
     //console.log(selection);
     for (i = 0; i < arrayLength; i++) {
 
@@ -179,13 +179,13 @@ function adddata(col) {
 
     } else {
 
-    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    if (isMobile) {
-      $("#wrapper").toggleClass("toggled");
-    }
+      var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+      if (isMobile) {
+        $("#wrapper").toggleClass("toggled");
+      }
 
-}
-document.getElementById("intro").innerHTML = "";
+    }
+    document.getElementById("intro").innerHTML = "";
 
   });
 
@@ -193,3 +193,191 @@ document.getElementById("intro").innerHTML = "";
 
 
 }
+
+
+
+
+
+
+
+
+
+//showing all entries in the homepage
+
+//PHASE I : RESPONSE READING STAGE ---------------------------------------------------------------
+//col = selected option from the side menu
+var response = [];
+
+new RGraph.Sheets('186WP_S-Th-njmMzMCq0dacfMTYU2MPumiT727llCXLI', function(sheet) {
+  var data = [];
+  // console.log("1 " +data);
+
+  for (row = 2; data !== null; row++) {
+    // loop through all rows in one column
+    data = sheet.get("C" + row); //data variable stores all the values from the selected single column
+    //traversing through the col until null value is reached, then break from loop
+    // console.log("2 = " +data);
+
+    if (data == null) {
+      break;
+    }
+    // console.log("3 = " +data);
+
+
+    response.push(data);
+  }
+
+
+  console.log("4 = " +response);
+
+  var arrayLength = response.length;
+  console.log("length = " + arrayLength);
+  //
+  // //PHASE I : RESPONSE READING STAGE END ---------------------------------------------------------------
+  //
+  // console.log("Array elements");
+  // console.log(data);
+  // indexpos = [];
+  // response.forEach(nameFilter);
+  //
+  //
+  // function nameFilter(value, index, array) {
+  //
+  //   if (value == "Yes") {
+  //     indexpos.push(index);
+  //     console.log(indexpos);
+  //   }
+  // }
+  //
+
+  //----------------------------------COLLECT NAMES WITH "YES" AS A REPLY-------------
+  names = [];
+  for (x = 2; x < arrayLength+2; x++) {
+    // loop through all rows in one column
+    name = sheet.get("C" + ([x])); //name variable stores all the name from the selected single column with a yes response
+    //traversing through the col until null value is reached, then break from loop
+    //console.log(name);
+    //console.log(data);
+    names.push(name);
+  }
+  console.log(names);
+  //----------------------------------COLLECT NAMES WITH "YES" AS A REPLY END-------
+
+  //----------------------------------COLLECT NUMBERS WITH "YES" AS A REPLY-------------
+  var numbers = [];
+  for (x = 2; x < arrayLength+2; x++) {
+    // loop through all rows in one column
+    number = sheet.get("D" + ([x])); //name variable stores all the name from the selected single column with a yes response
+    //traversing through the col until null value is reached, then break from loop
+    //console.log(number);
+    //console.log(data);
+    numbers.push(number);
+  }
+  console.log(numbers);
+  //----------------------------------COLLECT NUMBERS WITH "YES" AS A REPLY END-------
+
+
+  //----------------------------------COLLECT EMAIL WITH "YES" AS A REPLY-------------
+  var emails = [];
+  for (x = 2; x < arrayLength+2; x++) {
+    // loop through all rows in one column
+    email = sheet.get("B" + ([x])); //name variable stores all the name from the selected single column with a yes response
+    //traversing through the col until null value is reached, then break from loop
+    //console.log(email);
+    //console.log(data);
+    emails.push(email);
+  }
+  console.log(emails);
+  //----------------------------------COLLECT EMAIL WITH "YES" AS A REPLY END-------
+
+  //----------------------------------COLLECT COMPANY WITH "YES" AS A REPLY-------------
+  var companies = [];
+  for (x = 2; x < arrayLength+2; x++) {
+    // loop through all rows in one column
+    company = sheet.get("E" + ([x])); //name variable stores all the name from the selected single column with a yes response
+    //traversing through the col until null value is reached, then break from loop
+    //console.log(company);
+    //console.log(data);
+    companies.push(company);
+  }
+  console.log(companies);
+  //----------------------------------COLLECT COMPANY WITH "YES" AS A REPLY END-------
+
+  //----------------------------------COLLECT STATE WITH "YES" AS A REPLY-------------
+  var states = [];
+  for (x = 2; x < arrayLength+2; x++) {
+    // loop through all rows in one column
+    state = sheet.get("F" + ([x])); //name variable stores all the name from the selected single column with a yes response
+    //traversing through the col until null value is reached, then break from loop
+    //console.log(state);
+    //console.log(data);
+    states.push(state);
+  }
+  console.log(states);
+  //----------------------------------COLLECT STATE WITH "YES" AS A REPLY END-------
+
+  //----------------------------------COLLECT ROLE WITH "YES" AS A REPLY-------------
+  var roles = [];
+  for (x = 2; x < arrayLength+2; x++) {
+    // loop through all rows in one column
+    role = sheet.get("G" + ([x])); //name variable stores all the name from the selected single column with a yes response
+    //traversing through the col until null value is reached, then break from loop
+    //console.log(role);
+    //console.log(data);
+    roles.push(role);
+  }
+  console.log(roles);
+  //----------------------------------COLLECT ROLE WITH "YES" AS A REPLY END-------
+  //----------------------------------COLLECT ROLE WITH "YES" AS A REPLY-------------
+  var discs = [];
+  for (x = 2; x < arrayLength+2; x++) {
+    // loop through all rows in one column
+    disc = sheet.get("H" + ([x])); //name variable stores all the name from the selected single column with a yes response
+    //traversing through the col until null value is reached, then break from loop
+    //console.log(role);
+    //console.log(data);
+    discs.push(disc);
+  }
+  console.log(disc);
+  //----------------------------------COLLECT ROLE WITH "YES" AS A REPLY END-------
+
+  //console.log(selection);
+  for (i = 0; i < arrayLength; i++) {
+
+    var card = document.createElement('div');
+
+    card.innerHTML =
+     '<div class="col-md-4">' +
+
+      '<div class="thumbnail">' +
+      '<div class="card" style="width: 18rem;">' +
+      '<div class="card-body">' +
+      '<h5 class="card-title">' + names[i] + ' </h5>' +
+      '<h6 class="card-subtitle mb-2 text-muted">' + companies[i] + '</h6>' +
+      '<h6 class="card-subtitle mb-2 text-muted">' + numbers[i] + '</h6>' +
+      '<h6 class="card-subtitle mb-2 text-muted">' + emails[i] + '</h6>' +
+      '<p class="card-text">' + discs[i] + '</p>' +
+      '<a href="tel:' + numbers[i] + '" class="card-link">Call</a>' +
+      '<a href = "mailto: ' + emails[i] + '" class="card-link">Send Mail</a>' +
+      '</div>' +
+      '</div>' +
+      '</div>' +
+      '</div>';
+
+    document.getElementById("contactcard").appendChild(card);
+
+  }
+
+  if (arrayLength == 0) {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function() {
+      x.className = x.className.replace("show", "");
+    }, 2000);
+
+
+
+  }
+  // document.getElementById("intro").innerHTML = "";
+
+});
